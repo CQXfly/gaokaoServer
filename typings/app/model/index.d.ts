39abfbd8404d3362448efd163d770a8ebd5b38f0
chongqingxu <firefoxdance@163.com>
2019-03-28 19:25:23 +0800

    *. bugfix

--- typings/app/model/index.d.ts
+++ typings/app/model/index.d.ts
@@ -2,6 +2,7 @@
 // Do not modify this file!!!!!!!!!
 
 import 'egg'; // Make sure ts to import egg declaration at first
+import SchoolScore from '../../../app/model/SchoolScore';
 import AreaScore from '../../../app/model/areaScore';
 import MajorScore from '../../../app/model/majorScore';
 import Model from '../../../app/model/model';
@@ -11,11 +12,11 @@ import User from '../../../app/model/user';
 
 declare module 'sequelize' {
   interface Sequelize {
+    SchoolScore: ReturnType<typeof SchoolScore>;
     AreaScore: ReturnType<typeof AreaScore>;
     MajorScore: ReturnType<typeof MajorScore>;
     Model: ReturnType<typeof Model>;
     School: ReturnType<typeof School>;
-    SchoolScore: ReturnType<typeof SchoolScore>;
     User: ReturnType<typeof User>;
   }
 }
