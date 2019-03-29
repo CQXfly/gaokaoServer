975dc4ebdff04115dda0607411df06b0082c9daa
chongqingxu <firefoxdance@163.com>
2019-03-29 10:56:14 +0800

    不要js 了

--- app/controller/score.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic2NvcmUuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJzY29yZS50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIn0=
